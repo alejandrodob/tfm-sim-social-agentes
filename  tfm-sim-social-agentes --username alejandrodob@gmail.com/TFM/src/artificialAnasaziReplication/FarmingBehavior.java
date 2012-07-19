03a92177c920b2d74f5d763096c647d43a2d3111
alejandrodob <alejandrodob@395b30fe-7c62-bf4e-1e13-cd2b2b749c65>
2012-07-19 01:02:15 +0000

    lo mismo de hace 2 minutos, pero cambiando la mitad de los métodos del demographic en el farming que parecía el lugar apropiado, aunque aun habrá que ver las consecuencias de esto

---  tfm-sim-social-agentes --username alejandrodob@gmail.com/TFM/src/artificialAnasaziReplication/FarmingBehavior.java
+++  tfm-sim-social-agentes --username alejandrodob@gmail.com/TFM/src/artificialAnasaziReplication/FarmingBehavior.java
@@ -1,6 +1,9 @@
 package artificialAnasaziReplication;
 
+import java.util.Vector;
+
 import pruebaMADAM.DemographicBehavior;
+import sim.util.Int2D;
 import ec.util.MersenneTwisterFast;
 import model.SimpleWorld;
 import agent.DemographicItem;
@@ -18,7 +21,138 @@ public class FarmingBehavior implements BehaviorModule{
 	public static FarmingBehavior getInstance() {
 		return INSTANCE;
 	}
+	
+	private void moveHousehold(Household household, LongHouseValley valley, Int2D dest) {
+		valley.registerMigration(household, null, dest);
+	}
+	
+	private boolean findFarmAndSettlement(Household household, LongHouseValley valley) {
+		
+		//find a new spot for the settlement (might remain the same location as before)
+		boolean settlementFound = false;
+		int xh = 0;
+		int yh = 0;
+		
+		//if there are no potential farm spots available the agent is removed from the system
+		Vector<Int2D> potFarm = valley.determinePotentialFarms();
+		if (valley.getFarmSitesAvailable() > 0) {
+			Int2D bestFarm = household.determineBestFarm(potFarm);
+			double by = ((ValleyFloor) valley.getField()).plotAt(bestFarm.x,bestFarm.y).getYield();
+			//leave the current farm and move to the new better one
+			valley.changeFarmLocation(household, bestFarm);
+			
+			Vector<Int2D> potSettle = ((ValleyFloor) valley.getField()).potentialSettlements(by);
+			
+			//if there are cells with water which are not farmed and in a zone that is less productive than the zone where the favorite farm plot is located
+			if (potSettle.size() > 0) {
+				double minDist = Float.POSITIVE_INFINITY;
+				Int2D minSettle = null;
+				for (Int2D ps : potSettle) {
+					if (ValleyFloor.distance(bestFarm, ps) < minDist) {
+						minDist = ValleyFloor.distance(bestFarm, ps);
+						minSettle = ps;
+					}
+				}
+				if (minDist <= LongHouseValley.waterSourceDistance) {
+					xh = minSettle.x;
+					yh = minSettle.y;
+					settlementFound = true;
+				} else {
+					settlementFound = false;
+				}
+				if (settlementFound) {
+					potSettle = ((ValleyFloor) valley.getField()).potentialSettlementsRelaxed();
+					double minDist2 = Float.POSITIVE_INFINITY;
+					Int2D minSettle2 = null;
+					Int2D bestSett = new Int2D(xh,yh);
+					for (Int2D ps : potSettle) {
+						if (ValleyFloor.distance(bestSett, ps) < minDist2) {
+							minDist2 = ValleyFloor.distance(bestSett, ps);
+							minSettle2 = ps;
+						}
+					}
+					xh = minSettle2.x;
+					yh = minSettle2.y;
+					moveHousehold(household,valley,new Int2D(xh,yh));
+				}
+			}
+			
+			//if no settlement is found yet
+			if (!settlementFound) {
+				potSettle = ((ValleyFloor) valley.getField()).potentialSettlementsReRelaxed();
+				double minDist3 = Float.POSITIVE_INFINITY;
+				Int2D minSettle3 = null;
+				for (Int2D ps : potSettle) {
+					if (ValleyFloor.distance(bestFarm, ps) < minDist3) {
+						minDist3 = ValleyFloor.distance(bestFarm, ps);
+						minSettle3 = ps;
+					}
+				}
+				if (minDist3 <= LongHouseValley.waterSourceDistance) {
+					xh = minSettle3.x;
+					yh = minSettle3.y;
+					settlementFound = true;
+				} else {
+					settlementFound = false;
+				}
+				if (settlementFound) {
+					potSettle = ((ValleyFloor) valley.getField()).potentialSettlementsRelaxed();
+					double minDist4 = Float.POSITIVE_INFINITY;
+					Int2D minSettle4 = null;
+					Int2D bestSett = new Int2D(xh,yh);
+					for (Int2D ps : potSettle) {
+						if (ValleyFloor.distance(bestSett, ps) < minDist4) {
+							minDist4 = ValleyFloor.distance(bestSett, ps);
+							minSettle4 = ps;
+						}
+					}
+					xh = minSettle4.x;
+					yh = minSettle4.y;
+					moveHousehold(household,valley,new Int2D(xh,yh));
+				}
+			}
+			
+			// if not settlement found, don't worry about nearby watersources...
+			if (!settlementFound) {
+				potSettle = ((ValleyFloor) valley.getField()).potentialSettlementsReRelaxed();
+				double minDist3 = Float.POSITIVE_INFINITY;
+				Int2D minSettle3 = null;
+				for (Int2D ps : potSettle) {
+					if (ValleyFloor.distance(bestFarm, ps) < minDist3) {
+						minDist3 = ValleyFloor.distance(bestFarm, ps);
+						minSettle3 = ps;
+					}
+				}
+				xh = minSettle3.x;
+				yh = minSettle3.y;
+				settlementFound = true;
+				if (settlementFound) {
+					potSettle = ((ValleyFloor) valley.getField()).potentialSettlementsRelaxed();
+					double minDist4 = Float.POSITIVE_INFINITY;
+					Int2D minSettle4 = null;
+					Int2D bestSett = new Int2D(xh,yh);
+					for (Int2D ps : potSettle) {
+						if (ValleyFloor.distance(bestSett, ps) < minDist4) {
+							minDist4 = ValleyFloor.distance(bestSett, ps);
+							minSettle4 = ps;
+						}
+					}
+					xh = minSettle4.x;
+					yh = minSettle4.y;
+					moveHousehold(household,valley,new Int2D(xh,yh));
+				}
+			}
 
+			//if no possible settlement is found, leave the system
+			if (!settlementFound) {
+				//household.die(valley);
+			}
+		
+		} else {  //no farms available
+			//household.die(valley);
+		}
+		return settlementFound;
+	}
 	
 	private void harvestConsumption(Household household, LongHouseValley valley) {
 		
@@ -46,9 +180,16 @@ public class FarmingBehavior implements BehaviorModule{
 			ys--;
 		}
 	}
+	
 	@Override
 	public void behave(DemographicItem individual, SimpleWorld environment) {
-		harvestConsumption((Household) individual, (LongHouseValley) environment);
+		harvestConsumption((Household) individual,(LongHouseValley) environment);
+		((Household) individual).estimateHarvest();
+		//see if needs to move
+		if (((Household) individual).getEstimate() < ((Household) individual).getNutritionNeed()) {
+			if (findFarmAndSettlement((Household) individual, (LongHouseValley) environment))
+				System.out.println("exito al encontrar nueva granja y tal para no morir de hambre");
+		}
 	}
 
 }
