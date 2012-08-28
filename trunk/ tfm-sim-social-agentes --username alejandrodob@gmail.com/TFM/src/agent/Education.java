3f4e55005af7d9950d9e2389024879520df9e1d3
alejandrodob <alejandrodob@395b30fe-7c62-bf4e-1e13-cd2b2b749c65>
2012-08-28 08:04:40 +0000

    cambiado el argumento de entrada de los métodos de BasicSocialBehavior de Person, que es más específico, a Socializable, que es más general. Cambios en consecuencia en sus implementaciones en el ejemplo MADAM (castings sobre todo)

--- trunk/ tfm-sim-social-agentes --username alejandrodob@gmail.com/TFM/src/agent/Education.java
+++
@@ -1,9 +0,0 @@
-package agent;
-
-public interface Education {
-
-	// Un valor entre 0 y 1 que nos de una valoración del nivel educativo del
-	// individuo
-	public double nivelEduc();
-
-}
--- trunk/ tfm-sim-social-agentes --username alejandrodob@gmail.com/TFM/src/agent/behavior/BasicSocialBehavior.java
+++ trunk/ tfm-sim-social-agentes --username alejandrodob@gmail.com/TFM/src/agent/behavior/BasicSocialBehavior.java
@@ -2,26 +2,26 @@ package agent.behavior;
 
 import model.SimpleWorld;
 import agent.DemographicItem;
-import agent.Person;
+import agent.Socializable;
 
 public abstract class BasicSocialBehavior implements BehaviorModule {
 
 	@Override
 	public void behave(DemographicItem me, SimpleWorld environment) {
-		meetPeople((Person) me);
-		searchForMate((Person) me);
+		meetPeople((Socializable) me);
+		searchForMate((Socializable) me);
 	}
 
-	protected abstract void meetPeople(Person me);
+	protected abstract void meetPeople(Socializable me);
 
-	protected abstract void makeNewFriend(Person friend, Person me);
+	protected abstract void makeNewFriend(Socializable friend, Socializable me);
 
-	protected abstract void searchForMate(Person me);
+	protected abstract void searchForMate(Socializable me);
 
-	protected abstract void marry(Person partner, Person me);
+	protected abstract void marry(Socializable partner, Socializable me);
 	
-	public abstract boolean acceptFriend(Person friend, Person me);
+	public abstract boolean acceptFriend(Socializable friend, Socializable me);
 	
-	public abstract boolean acceptMarriage(Person candidate, Person me);
+	public abstract boolean acceptMarriage(Socializable candidate, Socializable me);
 
 }
